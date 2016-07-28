2cb51c9b9ac73a80b106fc919cb35317986f80ea
raunaak <rklohiya1996@gmail.com>
2016-07-29 00:21:54 +0530

    Create generic node for BST

---
+++ BNode.h
@@ -0,0 +1,27 @@
+#ifndef BNODE_H
+#define BNODE_H
+#include<cstddef>
+using namespace std;
+template<class T>
+class BNode{
+    public:
+        T data;        
+        BNode* left;
+        BNode* right;
+        BNode(T& data);
+        BNode(const T& data);
+};
+
+template<class T>
+BNode<T>::BNode(const T& data){
+    this->data = data;
+    left=right=NULL;
+}
+
+template<typename T>
+BNode<T>* newNode(const T& data){
+    BNode<T>* node = new BNode<T>(data);
+    return node;
+}
+#endif /* BNODE_H */
+
