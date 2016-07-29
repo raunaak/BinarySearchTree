2cf8c2ac79c516e5d6b4a3e8710df554631e9066
raunaak <rklohiya1996@gmail.com>
2016-07-29 11:36:22 +0530

    Add function to find lowest common ancestor in BST

---
+++ LowestCommonAncestor.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   LowestCommonAncestor.h
+ * Author: HP
+ *
+ * Created on 29 July, 2016, 11:33 AM
+ */
+
+#ifndef LOWESTCOMMONANCESTOR_H
+#define LOWESTCOMMONANCESTOR_H
+
+
+
+#endif /* LOWESTCOMMONANCESTOR_H */
+
