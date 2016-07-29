ef80bc824f0933a9ae9c2e49da7367d4c00ac0a0
raunaak <rklohiya1996@gmail.com>
2016-07-29 11:35:24 +0530

    Add function to check whether a tree is BST

---
+++ CheckForBST.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   CheckForBST.h
+ * Author: HP
+ *
+ * Created on 29 July, 2016, 11:33 AM
+ */
+
+#ifndef CHECKFORBST_H
+#define CHECKFORBST_H
+
+
+
+#endif /* CHECKFORBST_H */
+
