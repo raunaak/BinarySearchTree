97080a61cd6042b9ed91e560e9c12170e1a8c332
raunaak <rklohiya1996@gmail.com>
2016-07-31 13:01:57 +0530

    Add files via upload

--- LowestCommonAncestor.h
+++ LowestCommonAncestor.h
@@ -1,20 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   LowestCommonAncestor.h
- * Author: HP
- *
- * Created on 29 July, 2016, 11:33 AM
- */
-
 #ifndef LOWESTCOMMONANCESTOR_H
 #define LOWESTCOMMONANCESTOR_H
+#include"BNode.h"
+#include "SimpleOperationsOnBST.h"
+using namespace std;
 
+template<typename T> lca(BNode<T>* root, const T& key1, const T& key2){
+    if(root){
+        if((root->data-key1)*(root->data-key2)<=0)return root;
+        else if(root->data<key1)return lca(root->right, key1, key2);
+        else return lca(root->data>key2);
+    }
+}
 
+template<typename T>
+BNode<T>* lowestCommonAncestor(BNode<T>* root, const T& key1, const T& key2){
+    if(search(root, key1)&&search(root, key2))return lca(root, key1, key2);
+    return NULL;
+    
+}
 
 #endif /* LOWESTCOMMONANCESTOR_H */
-
